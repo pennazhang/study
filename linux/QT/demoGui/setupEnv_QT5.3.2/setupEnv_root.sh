4583ed4564bba5f93c5ff596c0626e0d701e4921
gogs <xing.liu@harman.com>
2020-03-02 14:05:25 +0000

    Add QT5.3.2 for Linux x86

---
+++ linux/QT/demoGui/setupEnv_QT5.3.2/setupEnv_root.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Run this script with sudo (called from install_dev)
+
+set -e
+MYDIR=`dirname "$0"`; MYDIR=`realpath "$MYDIR"`; export MYDIR
+
+cd "$MYDIR"
+
+# install dependencies
+if [ ! -f ~/.qt5_apt_done ]; then
+    # development
+    apt-get update
+    apt-get -y install iproute2 iputils-ping 
+    apt-get install -y git build-essential cmake autogen autoconf libtool m4 pkg-config bc device-tree-compiler python wget cpio unzip rsync gnupg2 dos2unix qtchooser
+    apt-get install -y locales && rm -rf /var/lib/apt/lists/* && localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
+    touch ~/.qt5_apt_done
+fi
--- linux/shellDebug/buildAll.sh
+++ linux/shellDebug/buildAll.sh
@@ -4,10 +4,22 @@ set -e
 
 CURRENT_DIR=`dirname "$0"`; CURRENT_DIR=`realpath "$CURRENT_DIR"`
 
-SOFTWARE_BASE=/git/junebox/software
-
+#for junebox Atmel ARM9G25.
+export SOFTWARE_BASE=/git/junebox/software
 export LD_LIBRARY_PATH=$SOFTWARE_BASE/cross_compiler/host/usr/lib/
 export PATH=$PATH:$SOFTWARE_BASE/cross_compiler/host/usr/bin/
 export CROSS_COMPILE=$SOFTWARE_BASE/cross_compiler/host/usr/bin/arm-linux-
+
+#for gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf
+#export SOFTWARE_BASE=/git/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf
+#export LD_LIBRARY_PATH=$SOFTWARE_BASE/arm-linux-gnueabihf/lib
+#export PATH=$PATH:$SOFTWARE_BASE/bin/
+#export CROSS_COMPILE=arm-linux-gnueabihf-
+
+#for /git/gcc-linaro-5.5.0-2017.10-x86_64_arm-linux-gnueabihf
+#export SOFTWARE_BASE=/git/gcc-linaro-5.5.0-2017.10-x86_64_arm-linux-gnueabihf
+#export LD_LIBRARY_PATH=$SOFTWARE_BASE/arm-linux-gnueabihf/lib
+#export PATH=$PATH:$SOFTWARE_BASE/bin/
+#export CROSS_COMPILE=arm-linux-gnueabihf-
 
 make all
