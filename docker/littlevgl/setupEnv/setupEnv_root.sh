155a4e836aebd4dafb1bea6116d0be9c00d231ee
pzhang <percy.zhang@harman.com>
2020-10-27 16:04:54 +0800

    import docker for littlevlg simulator

--- docker/littlevgl/setupEnv/setupEnv_root.sh
+++ docker/littlevgl/setupEnv/setupEnv_root.sh
@@ -12,7 +12,7 @@ if [ ! -f ~/.littlevgl_apt_done ]; then
     # development
     apt update || true
     apt install -y git iproute2 iputils-ping x11-utils dos2unix zip unzip  x11-apps wget cpio dos2unix iputils-ping net-tools iproute2 nano rsync
-    apt install -y build-essential libsdl2-dev
+    apt install -y build-essential libsdl2-dev cmake
     ln -s /git/study/bin/runCommand.sh /bin/runCommand.sh
     touch ~/.littlevgl_apt_done
 fi
