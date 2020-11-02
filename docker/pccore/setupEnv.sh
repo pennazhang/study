0df4ef1b1af4010a190f645f299cfcacd99aca50
pzhang <percy.zhang@harman.com>
2020-11-02 10:19:18 +0800

    modify docker for pccore

--- docker/pccore/setupEnv.sh
+++ docker/pccore/setupEnv.sh
@@ -21,15 +21,16 @@ cd $MYDIR
 sudo apt update
 #sudo bash -e "$MYDIR/setupEnv_root.sh" "$1"
 
-if [ ! -f ~/.debian_apt_done ]; then
+if [ ! -f /tmp/.debian_apt_done ]; then
     # common install
     sudo apt-get install -y wget cpio unzip rsync gnupg2 dos2unix iputils-ping net-tools iproute2 diffstat unzip texinfo chrpath nano
     sudo apt-get install -y git git-core  build-essential cmake autogen autoconf libtool m4 pkg-config bc device-tree-compiler libc6-i386 lib32stdc++6 lv2-c++-tools 
     sudo apt-get install -y python socat gawk doxygen
-    sudo apt-get install -y locales && localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
     
     cd $DOCKER_CACHE_DIR
-    dpkg -i  linux-headers-4.9.40-3-rt30_1.2.studer.infinity_amd64.deb
-    dpkg -i  linux-image-4.9.40-3-rt30_1.2.studer.infinity_amd64.deb
-    touch ~/.debian_apt_done
+    sudo dpkg -i  linux-headers-4.9.40-3-rt30_1.2.studer.infinity_amd64.deb
+    sudo dpkg -i  linux-image-4.9.40-3-rt30_1.2.studer.infinity_amd64.deb
+    touch /tmp/.debian_apt_done
 fi
+
+echo "setupEnv.sh is called successfully!"
