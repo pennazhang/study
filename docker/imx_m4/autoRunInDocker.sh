c18d4d3cb7865c5ccd6467f1b84975015e9ab06b
pzhang <percy.zhang@harman.com>
2020-12-07 14:27:06 +0800

    new modify

--- docker/imx_m4/autoRunInDocker.sh
+++ docker/imx_m4/autoRunInDocker.sh
@@ -1,3 +1,3 @@
-cd ~/workspace/jetstream
+cd ~/workspace/jetstream/IRResearch/imx_m4
 export ARMGCC_DIR=~/workspace/cacheData/gcc-arm-none-eabi-9-2019-q4-major
 export PATH=$PATH:~/workspace/cacheData/gcc-arm-none-eabi-9-2019-q4-major/bin
