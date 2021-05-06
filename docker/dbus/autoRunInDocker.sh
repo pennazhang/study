2a03145ee281166e78c4436140594d4fd371a3bb
pzhang <percy.zhang@harman.com>
2021-05-06 14:05:22 +0800

    Add dbus and jsoncpp to CPLUS_INCLUDE_PATH

--- docker/dbus/autoRunInDocker.sh
+++ docker/dbus/autoRunInDocker.sh
@@ -1,2 +1,2 @@
 cd ~/workspace/study/linux/dbus/sampledbus
-export CPLUS_INCLUDE_PATH=/usr/include/dbus-c++-1
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/usr/include/dbus-c++-1:/usr/include/jsoncpp
